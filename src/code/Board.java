0fbed6fe36d88ce2bbc6ac70c306305e0cb04f1a
Capriatto <michu-1@hotmail.com>
2016-05-18 19:41:19 -0500

    leyendo archivo de nivel 1 -Capriatto

--- src/code/Board.java
+++ src/code/Board.java
@@ -8,6 +8,12 @@ package code;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -35,6 +41,32 @@ public class Board implements KeyListener {
         caminoIcon = new ImageIcon(getClass().getResource("/recursos/caminoIcon.png"));
     }
 
+    public void mapeado(char caracter, int posicionX, int posicionY) {
+        // se parametrizan las imágenes
+//        
+//        botones[8][12].setIcon(muroIcon);
+//        botones[13][9].setIcon(cajaIcon);
+//        botones[9][15].setIcon(llegadaIcon);
+        switch (caracter) {
+            case 'M': //Muro (muro)
+                botones[posicionX][posicionY].setIcon(muroIcon);
+                break;
+            case 'B': // caja (box)
+                botones[posicionX][posicionY].setIcon(cajaIcon);
+                break;
+            case 'C': // camino
+                botones[posicionX][posicionY].setIcon(caminoIcon);
+                break;
+            case 'A': //avatar
+                botones[posicionX][posicionY].setIcon(avatarIcon);
+                break;
+            case 'L': //legada
+                 botones[posicionX][posicionY].setIcon(llegadaIcon);
+                break;
+
+        }
+    }
+
     public void matrizDeBotonesBloqueado(JPanel panel) {
         for (int i = 0; i < 20; i++) {
             a = i * 30;
@@ -44,16 +76,37 @@ public class Board implements KeyListener {
                 botones[i][j].setBounds(a, b, 30, 30);
                 botones[i][j].setEnabled(true);
                 botones[i][j].addKeyListener(this);
+                // se le da un ícono al fondo
                 botones[i][j].setIcon(caminoIcon);
                 panel.add(botones[i][j]);
             }
 
         }
-        botones[3][13].setIcon(avatarIcon);
-        botones[8][12].setIcon(muroIcon);
-        botones[13][9].setIcon(cajaIcon);
-        botones[9][15].setIcon(llegadaIcon);
+    }
 
+    public void leerArchivo(int nivel) {
+        String levelDirectory = System.getProperty("user.dir") + java.io.File.separator + "src/niveles" + java.io.File.separator;
+        String filename = levelDirectory + "nivel" + nivel + ".txt";
+
+        BufferedReader in;
+        try {
+            in = new BufferedReader(new FileReader(filename));
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot find file \"" + filename + "\".");
+            return;
+        }
+
+        try {
+            for (int i = 0; i < 20; i++) {
+                for (int j = 0; j < 20; j++) {
+                    mapeado((char) in.read(), j, i);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("File improperly formatted, quitting");
+            return;
+        }
     }
 
     @Override
@@ -85,7 +138,7 @@ public class Board implements KeyListener {
             } else if (botones[x][y - 1].getIcon() == llegadaIcon) {
                 botones[x][y].setIcon(caminoIcon);
                 botones[x][y - 1].setIcon(llegadaAvatarIcon);
-            } else if (botones[x][y].getIcon() == llegadaAvatarIcon && botones[x][y-1].getIcon() != cajaIcon && botones[x][y - 1].getIcon() != muroIcon && botones[x][y - 1].getIcon() != estrellaIcon) {
+            } else if (botones[x][y].getIcon() == llegadaAvatarIcon && botones[x][y - 1].getIcon() != cajaIcon && botones[x][y - 1].getIcon() != muroIcon && botones[x][y - 1].getIcon() != estrellaIcon) {
                 botones[x][y].setIcon(llegadaIcon);
                 botones[x][y - 1].setIcon(avatarIcon);
             } else if (botones[x][y - 1].getIcon() != estrellaIcon) {
@@ -93,7 +146,7 @@ public class Board implements KeyListener {
                 botones[x][y - 1].setIcon(avatarIcon);
             }
         } else if (e.VK_S == e.getKeyCode() && y < 19 && botones[x][y + 1].getIcon() != muroIcon) {
-                    if (botones[x][y + 1].getIcon() == cajaIcon && y <= 17 && botones[x][y + 2].getIcon() != muroIcon && botones[x][y + 2].getIcon() != llegadaIcon && botones[x][y].getIcon() != llegadaAvatarIcon) {
+            if (botones[x][y + 1].getIcon() == cajaIcon && y <= 17 && botones[x][y + 2].getIcon() != muroIcon && botones[x][y + 2].getIcon() != llegadaIcon && botones[x][y].getIcon() != llegadaAvatarIcon) {
                 botones[x][y + 1].setIcon(caminoIcon);
                 botones[x][y + 2].setIcon(cajaIcon);
                 botones[x][y].setIcon(caminoIcon);
@@ -113,7 +166,7 @@ public class Board implements KeyListener {
             } else if (botones[x][y + 1].getIcon() == llegadaIcon) {
                 botones[x][y].setIcon(caminoIcon);
                 botones[x][y + 1].setIcon(llegadaAvatarIcon);
-            } else if (botones[x][y].getIcon() == llegadaAvatarIcon && botones[x][y+1].getIcon() != cajaIcon && botones[x][y + 1].getIcon() != muroIcon && botones[x][y + 1].getIcon() != estrellaIcon) {
+            } else if (botones[x][y].getIcon() == llegadaAvatarIcon && botones[x][y + 1].getIcon() != cajaIcon && botones[x][y + 1].getIcon() != muroIcon && botones[x][y + 1].getIcon() != estrellaIcon) {
                 botones[x][y].setIcon(llegadaIcon);
                 botones[x][y + 1].setIcon(avatarIcon);
             } else if (botones[x][y + 1].getIcon() != estrellaIcon) {
@@ -131,7 +184,7 @@ public class Board implements KeyListener {
                 botones[x - 2][y].setIcon(estrellaIcon);
                 botones[x][y].setIcon(caminoIcon);
                 botones[x - 1][y].setIcon(avatarIcon);
-            }else if (botones[x - 1][y].getIcon() == cajaIcon && x >= 2 && botones[x - 2][y].getIcon() != muroIcon && botones[x - 2][y].getIcon() != llegadaIcon && botones[x][y].getIcon() == llegadaAvatarIcon) {
+            } else if (botones[x - 1][y].getIcon() == cajaIcon && x >= 2 && botones[x - 2][y].getIcon() != muroIcon && botones[x - 2][y].getIcon() != llegadaIcon && botones[x][y].getIcon() == llegadaAvatarIcon) {
                 botones[x - 1][y].setIcon(caminoIcon);
                 botones[x - 2][y].setIcon(cajaIcon);
                 botones[x][y].setIcon(llegadaIcon);
@@ -141,7 +194,7 @@ public class Board implements KeyListener {
             } else if (botones[x - 1][y].getIcon() == llegadaIcon) {
                 botones[x][y].setIcon(caminoIcon);
                 botones[x - 1][y].setIcon(llegadaAvatarIcon);
-            }  else if (botones[x][y].getIcon() == llegadaAvatarIcon && botones[x - 1][y].getIcon() != cajaIcon && botones[x - 1][y].getIcon() != muroIcon && botones[x - 1][y].getIcon() != estrellaIcon) {
+            } else if (botones[x][y].getIcon() == llegadaAvatarIcon && botones[x - 1][y].getIcon() != cajaIcon && botones[x - 1][y].getIcon() != muroIcon && botones[x - 1][y].getIcon() != estrellaIcon) {
                 botones[x][y].setIcon(llegadaIcon);
                 botones[x - 1][y].setIcon(avatarIcon);
             } else if (botones[x - 1][y].getIcon() != estrellaIcon) {
@@ -159,7 +212,7 @@ public class Board implements KeyListener {
                 botones[x + 2][y].setIcon(estrellaIcon);
                 botones[x][y].setIcon(caminoIcon);
                 botones[x + 1][y].setIcon(avatarIcon);
-            }else if (botones[x + 1][y].getIcon() == cajaIcon && x <= 17 && botones[x + 2][y].getIcon() != muroIcon && botones[x + 2][y].getIcon() != llegadaIcon && botones[x][y].getIcon() == llegadaAvatarIcon) {
+            } else if (botones[x + 1][y].getIcon() == cajaIcon && x <= 17 && botones[x + 2][y].getIcon() != muroIcon && botones[x + 2][y].getIcon() != llegadaIcon && botones[x][y].getIcon() == llegadaAvatarIcon) {
                 botones[x + 1][y].setIcon(caminoIcon);
                 botones[x + 2][y].setIcon(cajaIcon);
                 botones[x][y].setIcon(llegadaIcon);
@@ -169,7 +222,7 @@ public class Board implements KeyListener {
             } else if (botones[x + 1][y].getIcon() == llegadaIcon) {
                 botones[x][y].setIcon(caminoIcon);
                 botones[x + 1][y].setIcon(llegadaAvatarIcon);
-            }  else if (botones[x][y].getIcon() == llegadaAvatarIcon && botones[x + 1][y].getIcon() != cajaIcon && botones[x + 1][y].getIcon() != muroIcon && botones[x + 1][y].getIcon() != estrellaIcon) {
+            } else if (botones[x][y].getIcon() == llegadaAvatarIcon && botones[x + 1][y].getIcon() != cajaIcon && botones[x + 1][y].getIcon() != muroIcon && botones[x + 1][y].getIcon() != estrellaIcon) {
                 botones[x][y].setIcon(llegadaIcon);
                 botones[x + 1][y].setIcon(avatarIcon);
             } else if (botones[x + 1][y].getIcon() != estrellaIcon) {
