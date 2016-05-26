7deb209937920f2893534aa03835be0083b74b9c
BrayhanJaramillo <brayhanjaramillo@gmail.com>
2016-05-26 12:07:07 -0500

    Cambios coso

--- src/interfaz/Login.java
+++ src/interfaz/Login.java
@@ -47,12 +47,7 @@ public class Login extends javax.swing.JFrame {
         this.setUndecorated(true);
         initComponents();
         setLocationRelativeTo(this);
-        setResizable(false);
-        
-        String nombre="Brayhannivelbueno";
-        System.out.println("El coso es: " + nombre.startsWith(nombre));
-        
-        
+        setResizable(false); 
         if ((utilidades.cargarJugadores().size()) > 0) {
             jugador = utilidades.cargarJugadores();
         } else {
